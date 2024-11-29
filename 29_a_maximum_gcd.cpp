47ded98200cd372fbac9104d554ba5cb7987b918
MD ABDULLAH <110069678+CoderBDK@users.noreply.github.com>
2024-11-29 08:25:37 +0600

    Create 29_a_maximum_gcd.cpp

---
+++ 29_a_maximum_gcd.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <bitset>
+#include <set>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--) {
+       int n;
+       cin >> n;
+       cout << n /2 <<endl;
+    }
+    return 0;
+}
