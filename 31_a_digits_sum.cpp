6a36c0377b26b3eaac8b19210b5e97eeee18ad36
MD ABDULLAH <110069678+CoderBDK@users.noreply.github.com>
2024-12-01 06:38:54 +0600

    Create 31_a_digits_sum.cpp

---
+++ 31_a_digits_sum.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <bitset>
+#include <set>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int t = 0;
+    cin>>t;
+    while(t--) {
+        int n;
+        cin>>n;
+        cout << (n + 1) / 10 << endl;
+    }
+    return 0;
+}
