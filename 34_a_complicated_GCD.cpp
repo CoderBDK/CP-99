bd163f31ec9be6a3639f4c81e7de40c23309fc06
MD ABDULLAH <110069678+CoderBDK@users.noreply.github.com>
2024-12-04 23:12:15 +0600

    Create 34_a_complicated_GCD.cpp

---
+++ 34_a_complicated_GCD.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <bitset>
+#include <set>
+#include <cmath>
+
+using namespace std;
+
+typedef long long ll;
+#define MOD 100
+
+ll mod_mul(ll a, ll b, ll m = MOD) { return ((a % m) * (b % m)) % m; }
+ll mod_pow(ll a, ll b, ll m = MOD) {
+    ll res = 1;
+    while (b) {
+        if (b & 1) res = mod_mul(res, a, m);
+        a = mod_mul(a, a, m);
+        b >>= 1;
+    }
+    return res;
+}
+int main() {
+   string a,b;
+   cin>>a>>b;
+   if(a.size() == b.size()) {
+       cout <<a;
+   }else {
+       cout << 1;
+   }
+    return 0;
+}
