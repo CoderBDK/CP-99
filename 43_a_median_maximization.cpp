0216ab1fb2ca7d2b7219bb06a048a87234385541
MD ABDULLAH <110069678+CoderBDK@users.noreply.github.com>
2024-12-13 11:12:17 +0600

    Create 43_a_median_maximization.cpp

---
+++ 43_a_median_maximization.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <bitset>
+#include <set>
+#include <cmath>
+
+using namespace std;
+
+typedef long long ll;
+#define MOD 100
+
+ll mod_mul(ll a, ll b, ll m = MOD) { return ((a % m) * (b % m)) % m; }
+
+ll mod_pow(ll a, ll b, ll m = MOD) {
+    ll res = 1;
+    while (b) {
+        if (b & 1) res = mod_mul(res, a, m);
+        a = mod_mul(a, a, m);
+        b >>= 1;
+    }
+    return res;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, s;
+        cin >> n >> s;
+        int m = n / 2 + 1;
+        cout << s / m << endl;
+    }
+    return 0;
+}
