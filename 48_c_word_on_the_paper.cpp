a6b9d6cf394d163a478def85aa98647a9543a205
MD ABDULLAH <110069678+CoderBDK@users.noreply.github.com>
2024-12-18 09:37:11 +0600

    Create 48_c_word_on_the_paper.cpp

---
+++ 48_c_word_on_the_paper.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <bitset>
+#include <set>
+#include <cmath>
+
+using namespace std;
+
+typedef long long ll;
+#define MOD 100
+
+ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a % b); }
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+
+ll mod_mul(ll a, ll b, ll m = MOD) { return ((a % m) * (b % m)) % m; }
+
+ll mod_pow(ll a, ll b, ll m = MOD) {
+    ll res = 1;
+    while (b) {
+        if (b & 1) res = mod_mul(res, a, m);
+        a = mod_mul(a, a, m);
+        b >>= 1;
+    }
+    return res;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        for(int i = 0; i < 8; i++) {
+            string s;
+            cin>>s;
+            for(char c:s) {
+                if(c!='.') {
+                    cout << c;
+                }
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
