76b8e5416d9dd7d3b1799f2abb445b77ea540cf5
MD ABDULLAH <110069678+CoderBDK@users.noreply.github.com>
2024-12-29 07:45:05 +0600

    Create 59_a_domino_disaster.cpp

---
+++ 59_a_domino_disaster.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <bitset>
+#include <set>
+#include <cmath>
+
+using namespace std;
+
+typedef long long ll;
+#define MOD 100
+
+ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a % b); }
+
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+
+ll mod_mul(ll a, ll b, ll m = MOD) { return ((a % m) * (b % m)) % m; }
+
+ll mod_pow(ll a, ll b, ll m = MOD) {
+    ll res = 1;
+    while (b) {
+        if (b & 1) res = mod_mul(res, a, m);
+        a = mod_mul(a, a, m);
+        b >>= 1;
+    }
+    return res;
+}
+
+vector<bool> sieve(int n) {
+    vector<bool> isPrime(n + 1, true);
+    isPrime[0] = isPrime[1] = false;
+    for (int i = 2; i * i <= n; i++) {
+        if (isPrime[i]) {
+            for (int j = i * i; j <= n; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+    return isPrime;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        string s;
+        cin>>n>>s;
+        string r;
+        for (int i = 0; i < n; i++) {
+            char c = s[i];
+            if(c=='D') {
+                r.push_back('U');
+            }else if(c=='U') {
+                r.push_back('D');
+            }else {
+                r.push_back('L');
+                r.push_back('R');
+                i++;
+            }
+        }
+
+        cout << r << endl;
+    }
+    return 0;
+}
