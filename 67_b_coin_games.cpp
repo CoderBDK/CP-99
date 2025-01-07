b6bdc2db604ec379f87ca6b7bdeb221061bb75d2
MD ABDULLAH <110069678+CoderBDK@users.noreply.github.com>
2025-01-07 09:34:21 +0600

    Create 67_b_coin_games.cpp

---
+++ 67_b_coin_games.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <bitset>
+#include <set>
+#include <cmath>
+
+using namespace std;
+
+typedef long long ll;
+#define MOD 100
+
+ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a % b); }
+
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+
+ll mod_mul(ll a, ll b, ll m = MOD) { return ((a % m) * (b % m)) % m; }
+
+ll mod_pow(ll a, ll b, ll m = MOD) {
+    ll res = 1;
+    while (b) {
+        if (b & 1) res = mod_mul(res, a, m);
+        a = mod_mul(a, a, m);
+        b >>= 1;
+    }
+    return res;
+}
+
+vector<bool> sieve(int n) {
+    vector<bool> isPrime(n + 1, true);
+    isPrime[0] = isPrime[1] = false;
+    for (int i = 2; i * i <= n; i++) {
+        if (isPrime[i]) {
+            for (int j = i * i; j <= n; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+    return isPrime;
+}
+
+bool isVowel(char ch) {
+    ch = tolower(ch);
+    return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
+}
+
+int main() {
+    int t;
+    cin>>t;
+    while (t--) {
+        int n;
+        string s;
+        cin>>n>>s;
+
+        int uCount = 0;
+        for(char c:s) {
+            if(c == 'U') {
+                uCount++;
+            }
+        }
+
+        cout << (uCount & 1?"YES":"NO")<<endl;
+    }
+    return 0;
+}
