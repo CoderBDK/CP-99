d38dabfb4c91d26898ca9033b871593700927444
MD ABDULLAH <110069678+CoderBDK@users.noreply.github.com>
2025-01-09 10:49:19 +0600

    Create 69_b_two_gram.cpp

---
+++ 69_b_two_gram.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <bitset>
+#include <set>
+#include <cmath>
+
+using namespace std;
+
+typedef long long ll;
+#define MOD 100
+
+ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a % b); }
+
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+
+ll mod_mul(ll a, ll b, ll m = MOD) { return ((a % m) * (b % m)) % m; }
+
+ll mod_pow(ll a, ll b, ll m = MOD) {
+    ll res = 1;
+    while (b) {
+        if (b & 1) res = mod_mul(res, a, m);
+        a = mod_mul(a, a, m);
+        b >>= 1;
+    }
+    return res;
+}
+
+vector<bool> sieve(int n) {
+    vector<bool> isPrime(n + 1, true);
+    isPrime[0] = isPrime[1] = false;
+    for (int i = 2; i * i <= n; i++) {
+        if (isPrime[i]) {
+            for (int j = i * i; j <= n; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+    return isPrime;
+}
+
+bool isVowel(char ch) {
+    ch = tolower(ch);
+    return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
+}
+
+int main() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    unordered_map<string,int> map;
+
+    for (int i = 0; i < n - 1; ++i) {
+        string key = "";
+        key+=s[i];
+        key+=s[i + 1];
+        map[key]++;
+    }
+    string ans;
+    int temp = 0;
+
+    for(auto a:map) {
+        if(a.second >= temp) {
+            temp = a.second;
+            ans = a.first;
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
